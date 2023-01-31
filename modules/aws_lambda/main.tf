bac179723a945e9a1aa20d57044004c8cb8c9da1
André Deuerling <andre.deuerling@innoq.com>
2023-01-31 09:25:32 +0100

    Added REST info endpoint for data product

--- modules/aws_lambda/main.tf
+++ modules/aws_lambda/main.tf
@@ -1,43 +1,48 @@
 locals {
   aws_lambda_function_name = "${var.product.domain}_${var.product.name}"
 
-  out_directory       = "${path.root}/output"
-  out_product_archive = "archive_${var.product.domain}_${var.product.name}.zip"
+  transform_out_directory  = "${path.root}/out_transform"
+  transform_out_archive    = "archive_${var.product.domain}_${var.product.name}.zip"
+
+  info_out_directory       = "${path.root}/out_info"
+  info_out_archive         = "archive_${var.product.domain}_${var.product.name}-info.zip"
+
+  out_directory            = "${path.root}/out_archives"
 }
 
 resource "local_file" "query_to_s3" {
-  content = templatefile("${path.module}/templates/to-s3.sql.tftpl", {
+  content = templatefile("${path.module}/templates/transform.sql.tftpl", {
     name     = var.product.name
     location = "s3://${var.s3_bucket.bucket}/output/data/"
     format   = var.product.output.format
     query    = file("${path.cwd}/${var.product.transform.query}")
   })
-  filename = "${local.out_directory}/query_${var.product.domain}_${var.product.name}.sql"
+  filename = "${local.transform_out_directory}/query_${var.product.domain}_${var.product.name}.sql"
 }
 
 resource "local_file" "lambda_to_s3" {
-  content = templatefile("${path.module}/templates/lambda_function.py.tftpl", {
+  content = templatefile("${path.module}/templates/transform.py.tftpl", {
     name             = "query_${var.product.domain}_${var.product.name}"
     glue_database    = var.product.domain
     athena_workgroup = var.aws_athena_workgroup_id
     athena_catalog   = var.aws_athena_data_catalog_name
   })
-  filename = "${local.out_directory}/lambda_function.py"
+  filename = "${local.transform_out_directory}/lambda_function.py"
 }
 
 data "archive_file" "archive_to_s3" {
   type = "zip"
 
-  source_dir  = local.out_directory
-  output_path = "${local.out_directory}/${local.out_product_archive}"
+  source_dir  = local.transform_out_directory
+  output_path = "${local.out_directory}/${local.transform_out_archive}"
 
-  depends_on = [local_file.query_to_s3, local_file.lambda_to_s3]
+  depends_on = [ local_file.query_to_s3, local_file.lambda_to_s3 ]
 }
 
 resource "aws_s3_object" "archive_to_s3_object" {
   bucket = var.s3_bucket.bucket
 
-  key    = "lambdas/${local.out_product_archive}"
+  key    = "lambdas/${local.transform_out_archive}"
   source = data.archive_file.archive_to_s3.output_path
   etag   = data.archive_file.archive_to_s3.output_md5
 
@@ -177,4 +182,111 @@ resource "aws_cloudwatch_event_target" "aws_cloudwatch_event_target" {
   rule      = aws_cloudwatch_event_rule.aws_cloudwatch_event_rule.name
   target_id = aws_lambda_function.aws_lambda_function.id
   arn       = aws_lambda_function.aws_lambda_function.arn
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// INFO
+
+resource "local_file" "lambda_info_to_s3" {
+  content = templatefile("${path.module}/templates/info.js.tftpl", {
+    response_message = jsonencode({
+      domain = var.product.domain
+      name   = var.product.name
+      output = "s3://${var.s3_bucket.bucket}/output/data/"
+    })
+  })
+  filename = "${local.info_out_directory}/lambda_function.js"
+}
+
+data "archive_file" "archive_info_to_s3" {
+  type = "zip"
+
+  source_dir  = local.info_out_directory
+  output_path = "${local.out_directory}/${local.info_out_archive}"
+
+  depends_on = [ local_file.lambda_info_to_s3 ]
+}
+
+resource "aws_s3_object" "archive_info_to_s3_object" {
+  bucket = var.s3_bucket.bucket
+
+  key    = "lambdas/${local.info_out_archive}"
+  source = data.archive_file.archive_info_to_s3.output_path
+  etag   = data.archive_file.archive_info_to_s3.output_md5
+
+  depends_on = [ data.archive_file.archive_info_to_s3 ]
+}
+
+resource "aws_lambda_function" "lambda_info" {
+  function_name     = "${var.product.domain}_${var.product.name}_info"
+
+  s3_bucket         = var.s3_bucket.bucket
+  s3_key            = aws_s3_object.archive_info_to_s3_object.key
+  s3_object_version = aws_s3_object.archive_info_to_s3_object.version_id
+
+  runtime           = "nodejs12.x"
+  handler           = "lambda_function.handler"
+  source_code_hash  = data.archive_file.archive_info_to_s3.output_base64sha256
+
+  role = aws_iam_role.lambda_execution_role.arn
+}
+
+resource "aws_apigatewayv2_api" "lambda_info" {
+  name          = "${var.product.domain}_${var.product.name}_info"
+  protocol_type = "HTTP"
+}
+
+resource "aws_apigatewayv2_stage" "lambda_info_prod" {
+  api_id = aws_apigatewayv2_api.lambda_info.id
+
+  name        = "prod"
+  auto_deploy = true
+
+  access_log_settings {
+    destination_arn = aws_cloudwatch_log_group.lambda_info.arn
+
+    format = jsonencode({
+      requestId               = "$context.requestId"
+      sourceIp                = "$context.identity.sourceIp"
+      requestTime             = "$context.requestTime"
+      protocol                = "$context.protocol"
+      httpMethod              = "$context.httpMethod"
+      resourcePath            = "$context.resourcePath"
+      routeKey                = "$context.routeKey"
+      status                  = "$context.status"
+      responseLength          = "$context.responseLength"
+      integrationErrorMessage = "$context.integrationErrorMessage"
+    }
+    )
+  }
+}
+
+resource "aws_apigatewayv2_integration" "lambda_info" {
+  api_id = aws_apigatewayv2_api.lambda_info.id
+
+  integration_uri    = aws_lambda_function.lambda_info.invoke_arn
+  integration_type   = "AWS_PROXY"
+  integration_method = "POST"
+}
+
+resource "aws_apigatewayv2_route" "lambda_info" {
+  api_id = aws_apigatewayv2_api.lambda_info.id
+
+  route_key = "$default"
+  target    = "integrations/${aws_apigatewayv2_integration.lambda_info.id}"
+}
+
+resource "aws_cloudwatch_log_group" "lambda_info" {
+  name = "/aws/api_gw/${aws_apigatewayv2_api.lambda_info.name}"
+
+  retention_in_days = 30
+}
+
+resource "aws_lambda_permission" "lambda_info" {
+  statement_id  = "AllowExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.lambda_info.function_name
+  principal     = "apigateway.amazonaws.com"
+
+  source_arn = "${aws_apigatewayv2_api.lambda_info.execution_arn}/*/*"
 }
