4ba2642a7967d39b92bba1e80af6b3bbfb3b3c3f
André Deuerling <andre.deuerling@innoq.com>
2023-03-02 10:14:02 +0100

    Updated resource names

--- main.tf
+++ main.tf
@@ -19,9 +19,9 @@ module "aws_athena_glue" {
   source = "./modules/aws_athena_glue"
 
   s3_bucket                      = module.aws_s3.s3_bucket
-  aws_athena_data_catalog_name   = replace(var.domain, "_", "-")
-  aws_athena_workgroup_name      = replace(var.domain, "_", "-")
-  aws_glue_catalog_database_name = replace(var.domain, "_", "-")
+  aws_athena_data_catalog_name   = local.product_fqn
+  aws_athena_workgroup_name      = local.product_fqn
+  aws_glue_catalog_database_name = local.product_fqn
 
   table_name   = local.product_fqn
   table_schema = var.output.schema
